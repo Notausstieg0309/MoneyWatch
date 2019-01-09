ff36438ce7d3781db45bfd84678c7383fab2a692
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2019-01-09 13:19:24 +0100

    optimization and inline docu of JS code

--- moneywatch/static/common_materialize.js
+++ moneywatch/static/common_materialize.js
@@ -1,57 +1,55 @@
 
-
-  $(document).ready(function(){
-     M.AutoInit();
-  });
+// materialize: initialize JS addons
+$(document).ready(function(){
+    M.AutoInit();
+});
   
- $(function() {
-     
-      $("select[name='regular']").on("change", function() {
-       var optionSelected = $("option:selected", this);
+$(function() {
+    
+    // ruleset: hide/show additional inputs at ruleset add/change views
+    $("select[name='regular']").on("change", function() {
 
-       if(optionSelected.val() != "0") 
-       {
-        $('div#regular_form_fields:hidden').slideDown();
-        $('div#regular_form_fields input').prop("required", true);
-       }
-       else
-       {
-            console.log("u")
+        if($("option:selected", this).val() != "0") 
+        {
+            $('div#regular_form_fields:hidden').slideDown();
+            $('div#regular_form_fields input').prop("required", true);
+        }
+        else
+        {
             $('div#regular_form_fields:visible').slideUp();
             $('div#regular_form_fields input').prop("required", false);
-       }
-    });
-    
-    $('.num').each(function () {
-        var item = $(this).attr("num");
-
-        if (Number(item) < 0) {
-            $(this).addClass('negative');
-        }else{
-            $(this).addClass('positive');
         }
     });
     
-        $('div.category_container.no_subcategories').each(function() {
+    // general: colorize all numeric values 
+    $('.num').each(function () {
+        item = $(this);
+        
+        if (Number(item.attr("num")) < 0)
+        {
+            item.addClass('negative');
+        } 
+        else
+        {
+            item.addClass('positive');
+        }
+    });
+    
+    // overview: minimize categories that have no subcategories
+    $('div.category_container.no_subcategories').each(function() {
         $(this).children(".category_content").hide();
     });
     
-    
-    
+
+    // overview: enable collapse functionality of category container
     $('div.category_container:not(.overview)').each(function () {
         
-         var item = $(this)
-         
-         caption = $(item.children("div.category_header"));
-         
-         caption.click(function() {
-             
-            var item = $($(this).next(".category_content"));
-
-            item.slideToggle(200);
-        });
-        
-        caption.css("cursor", "pointer");
+        $(this).children("div.category_header")
+            .css("cursor", "pointer")
+            .click(function() {
+                $(this).next(".category_content").slideToggle(200);
+            })
+        ;
     });
- });
+});
  
