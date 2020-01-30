c7cbc12dcfbb3221cc8f04724e1fa95f2bada0e8
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-01-31 00:06:03 +0100

    Revert "objects.py: add rule update when transaction is inserted (untested)"
    This reverts commit 78805e0550a865079f5213ca55fac701d5cf13b9.

--- moneywatch/utils/objects.py
+++ moneywatch/utils/objects.py
@@ -11,7 +11,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import expression as fn
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 from sqlalchemy import orm
-from sqlalchemy.sql import collate
 
 db = SQLAlchemy()
 
@@ -751,13 +750,7 @@ class Transaction(db.Model):
           return Transaction.query.order_by(Transaction.date.desc()).first()
         
 
-@event.listens_for(Transaction, 'after_insert')
-def handle_before_insert(mapper, connection, item):
-    if item.rule_id is not None and item.rule is not None and item.rule.regular:
-        item.rule.updateNextDue(item.date, item.valuta)
-    
-    
-  
+
 #    _____  _                            _ _______                             _   _             
 #   |  __ \| |                          | |__   __|                           | | (_)            
 #   | |__) | | __ _ _ __  _ __   ___  __| |  | |_ __ __ _ _ __  ___  __ _  ___| |_ _  ___  _ __  
