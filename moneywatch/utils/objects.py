afe3565dca8d551983772446ca5876c78d3f46ca
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-04-05 11:25:25 +0200

    objects.py: round trend difference before storing to avoid float precision deviances

--- moneywatch/utils/objects.py
+++ moneywatch/utils/objects.py
@@ -662,7 +662,7 @@ def handle_before_insert(session, item):
                     # calculate trend compared to the latest transaction in the database
                     last_transaction = rule.last_transaction(item.date)
                     if last_transaction is not None:
-                        trend = item.valuta - last_transaction.valuta
+                        trend = round(item.valuta - last_transaction.valuta,2)
                         item.trend = trend if trend != 0 else None
                         current_app.logger.debug("calculated trend '%s' for transaction '%s' (%s) from %s" , item.trend, item.description, item.valuta, item.date)
 
