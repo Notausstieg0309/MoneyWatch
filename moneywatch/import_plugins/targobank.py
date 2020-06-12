6ac98111dbefb5594ab2be007cb8ab2d40b46a05
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-06-12 15:45:54 +0200

    targobank.py: adapt targobank import plugin to new CSV format

--- moneywatch/import_plugins/targobank.py
+++ moneywatch/import_plugins/targobank.py
@@ -2,10 +2,20 @@ import re
 
 def parse_csv(stream, name):
 
+    is_valuta_re = re.compile("^-?\d+[\.,]\d{2}$")
     items = stream.read().decode("latin-1").split('\r\n')
     
     result = []
     
+    
+    # check if csv file contains 2 columns for faluta
+    two_columns_valuta = False
+    
+    if items:
+        if len(items[0]) == 6: # 2 columns for positive and negative valuta
+            two_columns_valuta = True
+            
+        
     for item in items:
     
         if item.strip():
@@ -14,18 +24,27 @@ def parse_csv(stream, name):
             columns = re.split('"?;"?', item)
             
             date = columns[0]
-            full_text = columns[1]
-            valuta = columns[2]
-            account = normalize_iban(columns[5])
             
-            if re.match(r"^\d\d\.\d\d\.\d\d\d\d$", date): # german format
-                result_item['date'] = get_date_from_string(columns[0], '%d.%m.%Y')
-            elif re.match(r"^\d\d\/\d\d\/\d\d\d\d$", date): # american format
-                result_item['date'] = get_date_from_string(columns[0], '%m/%d/%Y')
+            full_text = None
+            valuta = None
+            
+            if two_columns_valuta:
+                full_text = columns[4]
+                if columns[2] == "" and is_valuta_re.match(columns[3]) is not None:
+                    valuta = columns[3]
+                elif columns[3] == "" and is_valuta_re.match(columns[2]) is not None:
+                    valuta = columns[2]
+                else:
+                    raise Exception("unable to determine valuta columns in two columns valuta mode")
+            else:
+                valuta = columns[2]
+                full_text = columns[3]
+                   
+            if re.match(r"^\d\d\.\d\d\.\d\d\d\d$", date): # european format
+                result_item['date'] = get_date_from_string(date, '%d.%m.%Y')
                 
             result_item['full_text'] = full_text
             result_item['valuta'] = float(valuta.replace(",","."))
-            result_item['account'] = account
             
             if result_item['valuta'] == 0 and (result_item['full_text'].startswith("RESERV") or result_item['full_text'].startswith("VORAUSDATIERTE")):
                 continue
