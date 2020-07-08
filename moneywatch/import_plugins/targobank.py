a2a117b0c76a493bb42954699e4d6b7551394e77
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-07-08 17:47:04 +0200

    targobank.py: fix two columns detection

--- moneywatch/import_plugins/targobank.py
+++ moneywatch/import_plugins/targobank.py
@@ -3,30 +3,47 @@ import re
 def parse_csv(stream, name):
 
     is_valuta_re = re.compile(r'^-?\d+[\.,]\d{2}$')
+
+    column_split_re = re.compile(r'["\']?;["\']?')
     items = stream.read().decode("latin-1").split('\r\n')
     
     result = []
-        
+
+    # check if csv file contains 2 columns for faluta
+    two_columns_valuta = False
+
+    if items:
+
+        columns = column_split_re.split(items[0])
+
+        if len(columns) == 7: # 2 columns for positive and negative valuta
+            two_columns_valuta = True    
+
     for item in items:
     
         if item.strip():
             result_item = {}
             
-            columns = re.split(r'["\']?;["\']?', item)
-            
+            columns = column_split_re.split(item)
+   
             date = columns[0]
             full_text = columns[1]
-            account = columns[5]
 
             valuta = None
-               
-            if columns[2] == "" and is_valuta_re.match(columns[3]) is not None:
-                valuta = columns[3]
-            elif columns[3] == "" and is_valuta_re.match(columns[2]) is not None:
-                valuta = columns[2]
-            else:
-                raise Exception("unable to determine valuta column")
             
+            if two_columns_valuta:
+                account = columns[6]
+
+                if columns[2] == "" and is_valuta_re.match(columns[3]) is not None:
+                    valuta = columns[3]
+                elif columns[3] == "" and is_valuta_re.match(columns[2]) is not None:
+                    valuta = columns[2]
+                else:
+                    raise Exception("unable to determine valuta column")
+            else:
+                valuta = columns[2]
+                account = columns[5]
+                
             if re.match(r"^\d\d\.\d\d\.\d\d\d\d$", date): # european format
                 result_item['date'] = get_date_from_string(date, '%d.%m.%Y')
                 
@@ -36,7 +53,7 @@ def parse_csv(stream, name):
             
             if result_item['valuta'] == 0 and (result_item['full_text'].startswith("RESERV") or result_item['full_text'].startswith("VORAUSDATIERTE")):
                 continue
-                
+            
             result.append(result_item)
 
     return result
