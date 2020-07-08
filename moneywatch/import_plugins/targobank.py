acaf891b85455b250567dd21b53a75318746ae1a
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-07-08 13:00:18 +0200

    targobank.py: adapt to changed Targobank CSV format
    - column order has changed
    - second date column is now gone
    - account IBAN is back again in static column index 5

--- moneywatch/import_plugins/targobank.py
+++ moneywatch/import_plugins/targobank.py
@@ -2,49 +2,37 @@ import re
 
 def parse_csv(stream, name):
 
-    is_valuta_re = re.compile("^-?\d+[\.,]\d{2}$")
+    is_valuta_re = re.compile(r'^-?\d+[\.,]\d{2}$')
     items = stream.read().decode("latin-1").split('\r\n')
     
     result = []
-    
-    
-    # check if csv file contains 2 columns for faluta
-    two_columns_valuta = False
-    
-    if items:
-        if len(items[0]) == 6: # 2 columns for positive and negative valuta
-            two_columns_valuta = True
-            
         
     for item in items:
     
         if item.strip():
             result_item = {}
             
-            columns = re.split('"?;"?', item)
+            columns = re.split(r'["\']?;["\']?', item)
             
             date = columns[0]
-            
-            full_text = None
+            full_text = columns[1]
+            account = columns[5]
+
             valuta = None
-            
-            if two_columns_valuta:
-                full_text = columns[4]
-                if columns[2] == "" and is_valuta_re.match(columns[3]) is not None:
-                    valuta = columns[3]
-                elif columns[3] == "" and is_valuta_re.match(columns[2]) is not None:
-                    valuta = columns[2]
-                else:
-                    raise Exception("unable to determine valuta columns in two columns valuta mode")
-            else:
+               
+            if columns[2] == "" and is_valuta_re.match(columns[3]) is not None:
+                valuta = columns[3]
+            elif columns[3] == "" and is_valuta_re.match(columns[2]) is not None:
                 valuta = columns[2]
-                full_text = columns[3]
-                   
+            else:
+                raise Exception("unable to determine valuta column")
+            
             if re.match(r"^\d\d\.\d\d\.\d\d\d\d$", date): # european format
                 result_item['date'] = get_date_from_string(date, '%d.%m.%Y')
                 
             result_item['full_text'] = full_text
             result_item['valuta'] = float(valuta.replace(",","."))
+            result_item['account'] = account
             
             if result_item['valuta'] == 0 and (result_item['full_text'].startswith("RESERV") or result_item['full_text'].startswith("VORAUSDATIERTE")):
                 continue
