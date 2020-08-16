423a63ccc5a64f7c0dd5c2335a77cd648c266f3a
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-08-16 20:47:07 +0200

    __init__.py: honor test config
    convert CRLF line ending to Unix style LF

--- moneywatch/__init__.py
+++ moneywatch/__init__.py
@@ -16,7 +16,10 @@ def create_app(test_config=None):
 
     app = Flask(__name__, instance_relative_config=True)
     
-    app.config.from_mapping(
+    if test_config is not None:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_mapping(
             SESSION_TYPE="filesystem",
             SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(app.instance_path,'db.sqlite'),
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
