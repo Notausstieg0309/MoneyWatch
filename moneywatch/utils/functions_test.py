a4a917ef3bb29eece44d9f9745bd8998857fcfec
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-08-16 23:43:47 +0200

    functions: add unit tests for get_number_of_months/add_months/substract_months

--- moneywatch/utils/functions_test.py
+++ moneywatch/utils/functions_test.py
@@ -3,6 +3,7 @@ from . import functions as utils
 import pytest
 import datetime
 
+# get_date_from_string()
 @pytest.mark.parametrize("input,format,expected", [
     ('2009-10-01', '%Y-%m-%d', datetime.date(2009, 10, 1)),
     ('2009.10.01', '%Y.%m.%d', datetime.date(2009, 10, 1)),
@@ -13,7 +14,7 @@ def test_get_date_from_string(input, format, expected):
     assert utils.get_date_from_string(input, format) == expected
 
 
-
+# get_quarter_from_date()
 @pytest.mark.parametrize("input,expected", [
     (datetime.date(2009, 1, 1), 1),
     (datetime.date(2009, 2, 2), 1),
@@ -32,6 +33,7 @@ def test_get_quarter_from_date(input, expected):
     assert utils.get_quarter_from_date(input) == expected
 
 
+# get_half_year_from_date()
 @pytest.mark.parametrize("input,expected", [
     (datetime.date(2009, 1, 1), 1),
     (datetime.date(2009, 2, 2), 1),
@@ -49,6 +51,7 @@ def test_get_quarter_from_date(input, expected):
 def test_get_half_year_from_date(input, expected):
     assert utils.get_half_year_from_date(input) == expected
 
+# get_first_day_of_month()
 @pytest.mark.parametrize("year,month,expected", [
     (None, None, datetime.date(datetime.date.today().year, datetime.date.today().month, 1) ),
     (2009, None, datetime.date(2009, datetime.date.today().month, 1) ),
@@ -59,7 +62,7 @@ def test_get_first_day_of_month(year, month, expected):
     assert utils.get_first_day_of_month(year, month) == expected
 
 
-
+# get_last_day_of_month()
 @pytest.mark.parametrize("year,month,expected", [
     (2020, 1, datetime.date(2020,1, 31) ),
     (2020, 2, datetime.date(2020,2, 29) ),
@@ -72,3 +75,48 @@ def test_get_last_day_of_month(year, month, expected):
     assert utils.get_last_day_of_month(year, month) == expected
 
 
+# get_number_of_months()
+@pytest.mark.parametrize("start,end,expected", [
+    (None, None, 1 ),
+    (datetime.date(2020,1, 1), datetime.date(2020, 1, 2),  1 ),
+    (datetime.date(2020,1, 1), datetime.date(2020, 1, 31), 1 ),
+    (datetime.date(2020,1, 1), datetime.date(2020, 2, 2),  2 ),
+    (datetime.date(2020,1, 1), datetime.date(2020, 12, 31),  12 ),
+    (datetime.date(2020,1, 1), datetime.date(2021, 1, 2),  13 ),
+])
+def test_get_number_of_months(start, end, expected):
+    assert utils.get_number_of_months(start, end) == expected
+
+
+# add_months()
+@pytest.mark.parametrize("start,months,expected", [
+    (datetime.date(2020,1, 1),  3,  datetime.date(2020, 4, 1) ),
+    (datetime.date(2020,1, 28), 1,  datetime.date(2020, 2, 28) ),
+    (datetime.date(2020,1, 29), 1,  datetime.date(2020, 2, 29) ),
+    (datetime.date(2020,1, 29), 12,  datetime.date(2021, 1, 29) ),
+    (datetime.date(2020,2, 29), 12,  datetime.date(2021, 2, 28) ),
+    (datetime.date(2020,1, 30), 1,  datetime.date(2020, 2, 29) ),
+    (datetime.date(2020,1, 30), 2,  datetime.date(2020, 3, 30) ),
+    (datetime.date(2020,1, 31), 2,  datetime.date(2020, 3, 31) ),
+    (datetime.date(2020,1, 31), 1,  datetime.date(2020, 2, 29) ),
+    (datetime.date(2020,1, 31), 2,  datetime.date(2020, 3, 31) ),
+    (datetime.date(2020,2, 29), 1,  datetime.date(2020, 3, 31) ),
+])
+def test_add_months(start, months, expected):
+    assert utils.add_months(start, months) == expected
+
+
+# substract_months()
+@pytest.mark.parametrize("start,months,expected", [
+    (datetime.date(2020, 4, 1),  3, datetime.date(2020,1, 1) ),
+    (datetime.date(2020, 2, 28), 1,  datetime.date(2020,1, 28) ),
+    (datetime.date(2020, 2, 29), 1,  datetime.date(2020,1, 31) ),
+    (datetime.date(2020, 3, 29), 1,  datetime.date(2020,2, 29) ),
+    (datetime.date(2020, 3, 29), 2,  datetime.date(2020,1, 29) ),
+    (datetime.date(2020, 3, 30), 1,  datetime.date(2020,2, 29) ),
+    (datetime.date(2020, 3, 31), 1,  datetime.date(2020,2, 29) ),
+    (datetime.date(2020, 3, 31), 2,  datetime.date(2020,1, 31) ),
+
+])
+def test_substract_months(start, months, expected):
+    assert utils.substract_months(start, months) == expected
