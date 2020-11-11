275ff5fd6b697285dd1d393d4b3e20a6da980a83
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2020-11-11 22:43:28 +0100

    analysis.js: white space cleanup

--- moneywatch/static/analysis.js
+++ moneywatch/static/analysis.js
@@ -12,22 +12,22 @@ function generateAnalysisChartData(res)
     var labels = []
     var data = []
     var pointColors = [];
-    
+
     var red = "#a82727";
     var green = "#045904";
 
-    $.each(res.data, function(index, item) 
-    { 
+    $.each(res.data, function(index, item)
+    {
         labels.push(item.label);
         data.push(item.valuta);
 
         if(item.valuta < 0) {
             pointColors.push(red);
         } else {
-            pointColors.push(green); 
-        }   
+            pointColors.push(green);
+        }
     });
-    
+
     return {labels: labels, data: data, pointColors: pointColors};
 }
 
@@ -39,14 +39,14 @@ function createTable(res) {
     var tr_template = table_el.children("template").html();
 
     $.each(res.data, function (index, item) {
-        
+
         var new_tr_el = $(tr_template);
-        
+
         new_tr_el.find(".label-value").html(item.label);
         new_tr_el.find(".valuta-value").html(item.valuta_formatted);
         new_tr_el.find(".valuta-value.num").data("num", item.valuta);
         new_tr_el.find(".count-value").html(item.count);
-      
+
         new_tr_el.insertBefore("table.analysis-table tr.sum");
     });
 
@@ -60,18 +60,18 @@ function createTable(res) {
 }
 
 // draw the chart based on the response data
-function createAnalysisChart(res) 
-{   
+function createAnalysisChart(res)
+{
         var canvas = $("canvas#analysis_chart")[0]
         var ctx = canvas.getContext('2d');
 
         var items = generateAnalysisChartData(res);
 
-        if( analysis_chart != null) 
+        if( analysis_chart != null)
         {
             analysis_chart.destroy();
         }
-        
+
 
         analysis_chart = new Chart(ctx, {
 
@@ -96,7 +96,7 @@ function createAnalysisChart(res)
             data: {
                 labels: items.labels,
                 datasets: [
-                            { 
+                            {
                                 label: res.description,
                                 data: items.data,
                                 pointBackgroundColor: items.pointColors,
@@ -114,9 +114,9 @@ function createAnalysisChart(res)
                             callback: function(value, index, values) {
                                         return value + ' €';
                                       }
-                        }   
+                        }
                     }],
-                    xAxes: [{ 
+                    xAxes: [{
                         ticks: {
                           fontColor: "#000",
                         },
@@ -125,7 +125,7 @@ function createAnalysisChart(res)
                 tooltips: {
                     callbacks: {
                         label: function(tooltipItem, chart){
-                          
+
                             return new Intl.NumberFormat(getLang(), { style: 'currency', currency: 'EUR' } ).format(tooltipItem.yLabel);
                         }
                     },
@@ -153,7 +153,7 @@ function showAllAccountOption()
      var allaccounts_el = select_el.children("option.all_accounts");
      allaccounts_el.removeAttr("disabled");
      allaccounts_el.show();
-     
+
      select_el.formSelect();
 }
 
@@ -162,14 +162,14 @@ function hideAllAccountOption()
 {
     var select_el = $("ul.criteria-container select#account");
     var allaccounts_el = select_el.children("option.all_accounts");
-    
+
     if( allaccounts_el.is(':checked') ) {
         select_el.children("option:first").prop("selected", "selected");
     }
 
     allaccounts_el.attr("disabled", true);
     allaccounts_el.hide();
-    
+
     select_el.formSelect();
 }
 
@@ -183,7 +183,7 @@ function setTimerangeControlsAndShow(start, end) {
     start_el.attr("max", end);
     end_el.attr("min", start);
     end_el.attr("max", end);
-    
+
     if(start_el.val() == "") {
         start_el.val(start);
     }
@@ -198,36 +198,36 @@ function setTimerangeControlsAndShow(start, end) {
 // create a javascript object with all query parameters based on the selected inputs.
 // only if the inputs are all valid, the object will be returned
 function createRequestDataObject() {
-    
+
     var selected_type_el = $('ul.criteria-container input:radio[name="type"]:checked');
-    
+
     var selected_type_value = selected_type_el.attr("value");
-    
+
     var selected_subtype_el = $('ul.criteria-container input:radio[name="subtype_'+selected_type_value+'"]:checked');
     var selected_subtype_value = selected_subtype_el.attr("value");
-    
+
     var selected_account_el = $("ul.criteria-container select#account option:checked:not([disabled])");
-    
+
     var selected_rule_el = $("ul.criteria-container select#rule option:checked:not([disabled])");
-    
+
     var selected_category_el = $("ul.criteria-container select#category option:checked:not([disabled])");
-    
+
     var timing_start_el = $("ul.criteria-container input#start:visible");
     var timing_end_el = $("ul.criteria-container input#end:visible");
-    
+
     var timing_interval_el = $('ul.criteria-container input:radio[name="interval"]:checked');
-    
+
     var result = {};
     var complete = true;
-    
+
     result["type"] = selected_type_value;
-    
+
     if(selected_type_value == "in" || selected_type_value == "out") {
 
         if(selected_subtype_el.length) {
-            
+
             result["subtype"] = selected_subtype_value;
-            
+
             if(selected_subtype_value == "rule") {
                 if( selected_rule_el.length) {
                     result["rule"]= selected_rule_el.attr("value");
@@ -235,9 +235,9 @@ function createRequestDataObject() {
                     complete = false;
                 }
             }
-            
+
             if(selected_subtype_value == "category") {
-                
+
                 if(selected_category_el.length) {
                     result["category"]= selected_category_el.attr("value");
                 } else {
@@ -248,68 +248,68 @@ function createRequestDataObject() {
             complete = false;
         }
     }
-    
+
     if(selected_account_el.length) {
        result["account_id"] =  selected_account_el.attr("value");
     } else {
         complete = false;
     }
-          
+
 
     if(timing_start_el.length && timing_start_el.val() != "") {
       result["start"] = timing_start_el.val();
     } else {
        complete = false;
     }
-    
+
     if(timing_end_el.length && timing_end_el.val() != "") {
       result["end"] = timing_end_el.val();
     } else {
        complete = false;
     }
-    
-    
+
+
     if(timing_interval_el.length) {
        result["interval"] =  timing_interval_el.attr("value");
     } else {
         complete = false;
-    }    
+    }
 
 
     if(complete) {
         return result;
     }
-    
-    return undefined; 
+
+    return undefined;
 }
 
-// handler for criteria inputs 
+// handler for criteria inputs
 function criteriaChangedHandler() {
-    
+
     var selected_type_el = $('ul.criteria-container input:radio[name="type"]:checked');
     var selected_type_value = selected_type_el.attr("value");
-    
+
     var selected_subtype_el = $('ul.criteria-container input:radio[name="subtype_'+selected_type_value+'"]:checked');
-    
+
     var selected_account_el = $("ul.criteria-container select#account option:checked:not([disabled])");
-    
+
     var selected_rule_el = $("ul.criteria-container select#rule option:checked:not([disabled])");
-    
+
     var selected_category_el = $("ul.criteria-container select#category option:checked:not([disabled])");
-    
+
     // when type is selected, show account dropdown and subtype radio buttons (for type "in" / "out")
     if(selected_type_el.length) {
-        
+
         $("ul.criteria-container > li#account").slideDown();
-    
+
         if(selected_type_el.is('#balance') || selected_type_el.is('#profit')) {
-                
+
             $("ul.criteria-container > li#subtype").slideUp();
             $("ul.criteria-container > li#rule").slideUp();
             $("ul.criteria-container > li#category").slideUp();
             $("ul.criteria-container  ul#subtype-menu-in").slideUp();
             $("ul.criteria-container  ul#subtype-menu-out").slideUp();
-            
+
             showAllAccountOption();
         }
         else if(selected_type_el.is('#in')) {
@@ -324,89 +324,89 @@ function criteriaChangedHandler() {
             $("ul.criteria-container > li#account option.all_accounts").hide();
             hideAllAccountOption();
         }
-    }       
-            
+    }
+
     // when a account is selected for type "in" / "out", show the optional category/rule selection
     if(selected_account_el.length && (selected_type_el.is("#in") || selected_type_el.is("#out") ) ) {
-        
+
         var account_start = selected_account_el.data("start");
         var account_end = selected_account_el.data("end");
-                    
+
         if(selected_subtype_el.length) {
-            
+
             if(selected_subtype_el.is("#rule")) {
                 // hide category selection and show the rule selection
-                $("ul.criteria-container > li#category").slideUp();  
+                $("ul.criteria-container > li#category").slideUp();
                 showRuleSelect(selected_account_el.attr("value"), selected_type_el.attr("value"));
-                
+
                 if(selected_rule_el.length) {
                     var start = new Date(account_start);
                     var end = new Date(account_end);
-                    
+
                     var rule_start = selected_rule_el.data("start");
                     var rule_end = selected_rule_el.data("end");
-                    
+
                     // adapt the maximum timerange (start/end) to the rule timerange if it's smaller
                     if(rule_start != undefined) {
                         rule_start = new Date(rule_start);
-                        
+
                         if(rule_start > start) {
                             start = rule_start
                         }
                     }
-                    
+
                     if(rule_end != undefined) {
                         rule_end = new Date(rule_end);
                         if(rule_end < end) {
                             end = rule_end
                         }
                     }
-                    
+
                     setTimerangeControlsAndShow(formatYearMonth(start),formatYearMonth(end));
-                } 
+                }
             } else if(selected_subtype_el.is("#category")) {
                 // hide rule selection and show the category selection
-                $("ul.criteria-container > li#rule").slideUp();  
+                $("ul.criteria-container > li#rule").slideUp();
                 showCategorySelect(selected_account_el.attr("value"), selected_type_el.attr("value"));
-                
+
                 if(selected_category_el.length) {
                     setTimerangeControlsAndShow(account_start, account_end);
-                }   
+                }
 
             } else if(selected_subtype_el.is("#overall")) {
                 setTimerangeControlsAndShow(account_start, account_end);
-                $("ul.criteria-container > li#rule").slideUp();  
-                $("ul.criteria-container > li#category").slideUp();  
+                $("ul.criteria-container > li#rule").slideUp();
+                $("ul.criteria-container > li#category").slideUp();
             } else {
-              $("ul.criteria-container > li#rule").slideUp();  
-              $("ul.criteria-container > li#category").slideUp();  
+              $("ul.criteria-container > li#rule").slideUp();
+              $("ul.criteria-container > li#category").slideUp();
             }
         } else {
-          $("ul.criteria-container > li#rule").slideUp();  
-          $("ul.criteria-container > li#category").slideUp();  
+          $("ul.criteria-container > li#rule").slideUp();
+          $("ul.criteria-container > li#category").slideUp();
         }
     } else {
-         $("ul.criteria-container > li#rule").slideUp();  
-         $("ul.criteria-container > li#category").slideUp();  
+         $("ul.criteria-container > li#rule").slideUp();
+         $("ul.criteria-container > li#category").slideUp();
     }
-    
-    
+
+
     if(selected_account_el.length && (selected_type_el.is("#balance") || selected_type_el.is("#profit") ) ) {
-        
+
         var start = selected_account_el.data("start");
         var end = selected_account_el.data("end");
-        
+
         setTimerangeControlsAndShow(start, end);
     }
-    
-    
+
+
     // check for completeness and enable the submit button
     if(createRequestDataObject() !== undefined) {
         $("ul.criteria-container button#submit").removeClass("disabled");
     } else {
-       $("ul.criteria-container button#submit").addClass("disabled"); 
-    } 
-    
+       $("ul.criteria-container button#submit").addClass("disabled");
+    }
+
 }
 
 // generate the rule select dropdown menu by request the data via AJAX
@@ -414,29 +414,29 @@ function showRuleSelect(account_id, type) {
 
     var select_el = $("ul.criteria-container > li#rule select#rule");
     var select_container = select_el.parents(".input-field");
-      
+
     var spinner_el = $("ul.criteria-container > li#rule div.circle-spinner");
-    
+
     var current_account_id = select_el.data("account_id");
     var current_type = select_el.data("type");
-    
+
     if(current_account_id != account_id || current_type != type) {
-        
+
         spinner_el.show()
         select_container.hide();
-        
+
         $("ul.criteria-container > li#rule").slideDown();
-        
+
         $.getJSON($SCRIPT_ROOT + "/analysis/rules/" + account_id + "/" + type + "/", function (data, textStatus) {
-           
-           if(Array.isArray(data)) {           
+
+           if(Array.isArray(data)) {
                  select_el.children("option:not(:first)").remove();
-                 
+
                $.each(data, function(index, item) {
                   var new_el = $("<option></option>");
 
                   new_el.attr("value", item.id);
-                  
+
                   if(item.disabled) {
                     new_el.prop("disabled", true);
                   }
@@ -445,19 +445,19 @@ function showRuleSelect(account_id, type) {
                     new_el.data("end", item.end);
                   }
                   new_el.html(item.name);
-                  
+
                   select_el.append(new_el);
                });
-               
+
                select_el.data("account_id", account_id);
                select_el.data("type", type);
-               
-               select_el.formSelect();   
+
+               select_el.formSelect();
                spinner_el.hide();
                select_container.show();
            }
         });
-    
+
     } else {
         $("ul.criteria-container > li#rule").slideDown();
     }
@@ -469,42 +469,42 @@ function showCategorySelect(account_id, type) {
 
     var select_el = $("ul.criteria-container > li#category select#category");
     var select_container = select_el.parents(".input-field");
-      
+
     var spinner_el = $("ul.criteria-container > li#category div.circle-spinner");
-    
+
     var current_account_id = select_el.data("account_id");
     var current_type = select_el.data("type");
-    
+
     if(current_account_id != account_id || current_type != type) {
-        
+
         spinner_el.show()
         select_container.hide();
-        
+
         $("ul.criteria-container > li#category").slideDown();
-        
+
         $.getJSON($SCRIPT_ROOT + "/analysis/categories/" + account_id + "/" + type + "/", function (data, textStatus) {
-           
-           if(Array.isArray(data)) {           
+
+           if(Array.isArray(data)) {
                 select_el.children("option:not(:first)").remove();
-                 
+
                $.each(data, function(index, item) {
                   var new_el = $("<option></option>");
 
                   new_el.attr("value", item.id);
                   new_el.html(item.path);
-                  
+
                   select_el.append(new_el);
                });
-               
+
                select_el.data("account_id", account_id);
                select_el.data("type", type);
-               
-               select_el.formSelect();   
+
+               select_el.formSelect();
                spinner_el.hide();
                select_container.show();
            }
         });
- 
+
     } else {
         $("ul.criteria-container > li#category").slideDown();
     }
@@ -518,17 +518,17 @@ $(function () {
         criteriaChangedHandler();
     });
 
-    
+
     $('ul.criteria-container button#submit').click(function () {
-        
+
         var params = createRequestDataObject();
-        
-        if(params !== undefined) {            
-            
+
+        if(params !== undefined) {
+
             var canvas = $("canvas#analysis_chart")[0]
             var ctx = canvas.getContext('2d');
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            
+
             $.ajax({
                 url: $SCRIPT_ROOT + "/analysis/data/",
                 async: true,
@@ -538,7 +538,7 @@ $(function () {
             }).done(function (res) {
 
                 var data = generateAnalysisChartData(res);
-          
+
                 createAnalysisChart(res);
                 createTable(res);
                 $("div.chart-spinner").hide()
@@ -564,5 +564,5 @@ $(function () {
         $("ul.analysis-tabs li.disabled").removeClass("disabled");
 
         $("ul.analysis-tabs").tabs("select", "chart");
-    });    
+    });
 });
