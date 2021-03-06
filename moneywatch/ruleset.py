01f2ac22959f190e6bb99942eebf675aa093a274
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2021-03-06 12:39:45 +0100

    ruleset: rework add method

--- moneywatch/ruleset.py
+++ moneywatch/ruleset.py
@@ -34,87 +34,88 @@ def add(account_id, rule_type):
         flash(gettext("Unable to create new rules. No categories are available to create rules for. Please create categories first."))
         return redirect(url_for('ruleset.index', account_id=account.id))
 
-    if request.method == 'POST':
+    if request.method != 'POST':
+        return render_template('ruleset/add.html', account=account, rule_type=rule_type, categories=categories)
 
-        name = request.form['name'].strip()
-        pattern = request.form['pattern']
-        description = request.form['description'].strip()
-        regular = request.form['regular']
-        next_valuta = request.form.get('next_valuta', None)
-        next_due = request.form.get('next_date', None)
-        category_id = request.form.get('category_id', None)
+    name = request.form['name'].strip()
+    pattern = request.form['pattern']
+    description = request.form['description'].strip()
+    regular = request.form['regular']
+    next_valuta = request.form.get('next_valuta', None)
+    next_due = request.form.get('next_date', None)
+    category_id = request.form.get('category_id', None)
 
-        valid_pattern = True
+    valid_pattern = True
 
-        errors = []
+    errors = []
 
-        if not name:
-            errors.append(gettext('Rule name is required.'))
+    if not name:
+        errors.append(gettext('Rule name is required.'))
 
-        if not pattern:
-            errors.append(gettext('Search pattern is required.'))
+    if not pattern:
+        errors.append(gettext('Search pattern is required.'))
 
-        if not category_id:
-            errors.append(gettext('Category is required.'))
+    if not category_id:
+        errors.append(gettext('Category is required.'))
 
-        try:
-            re.compile(pattern)
-        except re.error:
-            valid_pattern = False
-            errors.append(gettext("Invalid search pattern. The given search pattern is not a valid regular expression"))
+    try:
+        re.compile(pattern)
+    except re.error:
+        valid_pattern = False
+        errors.append(gettext("Invalid search pattern. The given search pattern is not a valid regular expression"))
 
 
-        if next_due.strip() != "":
-            next_due = utils.get_date_from_string(next_due, "%Y-%m-%d")
-        else:
-            next_due = None
+    if next_due.strip() != "":
+        next_due = utils.get_date_from_string(next_due, "%Y-%m-%d")
+    else:
+        next_due = None
 
-        if next_valuta.strip() == "":
-            next_valuta = None
+    if next_valuta.strip() == "":
+        next_valuta = None
 
-        matched_transactions = []
-        selected_transaction_ids = request.form.getlist("matched_transactions")
+    matched_transactions = []
+    selected_transaction_ids = request.form.getlist("matched_transactions")
 
-        if len(errors) == 0:
+    if len(errors) != 0:
+        for error in errors:
+            flash(error)
 
-            item = {}
+    else:
+        item = {}
 
-            item["type"] = rule_type
-            item["name"] = name
-            item["pattern"] = pattern
-            item["description"] = description
-            item["category_id"] = category_id
-            item["account_id"] = account.id
-            item["regular"] = regular
+        item["type"] = rule_type
+        item["name"] = name
+        item["pattern"] = pattern
+        item["description"] = description
+        item["category_id"] = category_id
+        item["account_id"] = account.id
+        item["regular"] = regular
 
-            item["next_due"] = next_due
-            item["next_valuta"] = next_valuta
+        item["next_due"] = next_due
+        item["next_valuta"] = next_valuta
 
-            new_rule = Rule(**item)
+        new_rule = Rule(**item)
 
-            if request.form.get("check_historical", None) == "on" and valid_pattern:
+        if request.form.get("check_historical", None) == "on" and valid_pattern:
 
-                for transaction in account.transactions_by_type(rule_type):
-                    if new_rule.match_transaction(transaction):
-                        matched_transactions.append(transaction)
+            for transaction in account.transactions_by_type(rule_type):
+                if new_rule.match_transaction(transaction):
+                    matched_transactions.append(transaction)
 
-            if request.form['action'] == "save":
+        if request.form['action'] == "save":
 
-                db.session.add(new_rule)
+            db.session.add(new_rule)
+            db.session.commit()
+
+            if request.form.get("check_historical", None) == "on" and len(selected_transaction_ids) > 0:
+                new_rule.assign_transaction_ids(selected_transaction_ids)
                 db.session.commit()
 
-                if request.form.get("check_historical", None) == "on" and len(selected_transaction_ids) > 0:
-                    new_rule.assign_transaction_ids(selected_transaction_ids)
-                    db.session.commit()
+            return redirect(url_for('ruleset.index', account_id=account.id))
 
-                return redirect(url_for('ruleset.index', account_id=account.id))
-        else:
-            for error in errors:
-                flash(error)
+    return render_template('ruleset/check.html', account=account, rule_type=rule_type, categories=categories, matched_transactions=matched_transactions, selected_transaction_ids=selected_transaction_ids)
 
-        return render_template('ruleset/check.html', account=account, rule_type=rule_type, categories=categories, matched_transactions=matched_transactions, selected_transaction_ids=selected_transaction_ids)
 
-    return render_template('ruleset/add.html', account=account, rule_type=rule_type, categories=categories)
 
 
 @bp.route('/ruleset/delete/<int:id>/')
